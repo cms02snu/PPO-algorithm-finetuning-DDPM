1690815e825fdbc46db11bce24fe6b82ef612440
cms02snu <163644701+cms02snu@users.noreply.github.com>
2025-11-03 10:19:52 +0900

    Delete ddpm-ppo-finetune-verbatim directory

--- ddpm-ppo-finetune-verbatim/src/ddpmppo/ppo/objective.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def ppo_clip_objective(ratio, adv, eps=0.2):
-    unclipped = ratio * adv
-    clipped = torch.clamp(ratio, 1.0 - eps, 1.0 + eps) * adv
-    loss = -torch.mean(torch.minimum(unclipped, clipped))  # gradient descent
-    return loss
--- ddpm-ppo-finetune-verbatim/src/ddpmppo/ppo/ratio.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def compute_log_ratio(logprob_new, logprob_old):
-    return (logprob_new - logprob_old)
-
-def clip_ratio(r, eps=0.2):
-    return torch.clamp(r, 1.0 - eps, 1.0 + eps)
